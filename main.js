e77ed5d99d98557c85eeddc48497414cb65a03f2
Mohamed-Waled <80546573+Mohamed-Waled@users.noreply.github.com>
2022-07-14 01:10:27 +0200

    All Project files

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let header = document.querySelector("header");
-let scroll1 = window.screenY;
+let scroll1 = window.scrollY;
 
 document.addEventListener("scroll", () => {
-  if (scroll1 > this.scrollY) {
-    header.style.top = "0px";
-    scroll1 = this.scrollY;
-  } else {
-    header.style.top = "-100px";
-    scroll1 = this.scrollY;
+  if (this.scrollY >= 160 && this.screenY <= 1500) {
+    if (scroll1 > this.scrollY) {
+      header.style.top = "0px";
+      scroll1 = this.scrollY;
+    } else {
+      header.style.top = "-100px";
+      scroll1 = this.scrollY;
+    }
   }
 });
